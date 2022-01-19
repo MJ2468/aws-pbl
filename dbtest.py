7ef1074c5f3b090a653d90f50f07f88c1c35f9e4
minji <97005604+MJ2468@users.noreply.github.com>
2022-01-19 09:22:29 +0900

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -1,3 +1,3 @@
 import pymysql
-conn = pymysql.connect(host = 'database-1.c2zw45njcc7u.ap-northeast-2.rds.amazonaws.com', db='pbldb', port=3306,passwd='pblworking1234', user='admin')
+conn = pymysql.connect(host = '', db='pbldb', port=3306,passwd='', user='admin')
 print(conn)
