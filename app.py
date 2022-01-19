cf16c6bada92b9858f06dba6abbc0675abc495be
minji <govlalswl1@naver.com>
2022-01-19 14:05:08 +0900

    add .

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  # route는 url 생성하는 함수 ## /만 쓴거는 ip만 써도 나오는 기본 주소
 def hell():
-    return '<h1> Hello Flask!! <h1>'
+    return '<h1> Hello Flask!! <h1>' # 내가 보내야할 정보가 있다면 return 값에 넣어주기
+
+@app.route("/hello")
+def hi():
+    name1 = "mango1"
+    return render_template('hello.html', name = name1)  # http://172.20.40.79:8080 이 뒤에 /hello 를 적어줘야 열림
 
 @app.route('/test')
 def test():
