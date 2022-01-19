548d15b839710e08d8bff13f66a5e1d9615530fc
minji <govlalswl1@naver.com>
2022-01-19 13:04:45 +0900

    add .

--- dbtest.py
+++ dbtest.py
@@ -1,3 +1,11 @@
 import pymysql
-conn = pymysql.connect(host = 'database-1.c2zw45njcc7u.ap-northeast-2.rds.amazonaws.com', db='pbldb', port=3306,passwd='pblworking1234', user='admin')
+import boto3
+
+ssm = boto3.client('ssm')
+parameter = ssm.get_parameter(Name='/myweb/database1_password', WithDecryption=True)
+db_password = parameter['Parameter']['Value']
+
+# parameter로 db 비밀번호 설정하기
+conn = pymysql.connect(host = 'database-1.c2zw45njcc7u.ap-northeast-2.rds.amazonaws.com', db='pbldb',
+                       port=3306, passwd=db_password, user='admin')
 print(conn)
